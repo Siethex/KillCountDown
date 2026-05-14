ab1ab772226680c3498616c2aeb7571b774a0d05
Siethex <brygordon42@gmail.com>
2026-05-13 20:59:40 -0400

    Updated Interface to better reflect 2026WoWHC

---
+++ stubs.lua
@@ -0,0 +1,12 @@
+-- WOW API stubs for VS Code Lua Diag
+
+--Frame API
+function CreateFrame() return{} end
+--Unit API
+function UnitXP(unit) return 0 end
+function UnitXPMax(unit) return 0 end
+
+--Debugging
+function print(...)
+
+end
